e58c32727ec79a5baa0504f7afabbdd3378da7bd
JugolKarmakar <jugol.cse@gmail.com>
2022-05-27 22:19:54 +0600

    dashboard commit

--- index.js
+++ index.js
@@ -99,6 +99,13 @@ async function run() {
       res.send(users);
     });
 
+    app.get("/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({ email: email });
+      const isAdmin = user.role === "admin";
+      res.send({ admin: isAdmin });
+    });
+
     // users admin
     app.put("/user/admin/:email", verifyJWT, async (req, res) => {
       const email = req.params.email;
