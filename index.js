3d9fb7954422c96a89a776f8465db53d879b0e88
JugolKarmakar <jugol.cse@gmail.com>
2022-11-26 02:13:16 +0600

    dashboard commit

--- index.js
+++ index.js
@@ -59,65 +59,19 @@ async function run() {
       res.send(purchase);
     });
 
-    // parts post
-    app.post("/parts", async (req, res) => {
-      const parts = req.body;
-      const result = await partsCollection.insertOne(parts);
-      res.send(result);
-    });
-
-    // parts booking get
-    app.get("/booking", verifyJWT, async (req, res) => {
-      const email = req.query.email;
-      const decodedEmail = req.decoded.email;
-      if (email === decodedEmail) {
-        const query = { email: email };
-        const bookings = await bookingCollection.find(query).toArray();
-        return res.send(bookings);
-      } else {
-        return res.status(403).send({ message: "Forbiden Access" });
-      }
-    });
-
-    // delete parts
-
-    app.delete("/parts/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const result = await partsCollection.deleteOne(query);
-      res.send(result);
-    });
-
-    app.get("/booking/:id", verifyJWT, async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const booking = await bookingCollection.findOne(query);
-      res.send(booking);
-    });
-
-    app.patch("/booking/:id", verifyJWT, async (req, res) => {
-      const id = req.params.id;
-      const payment = req.body;
-      const filter = { _id: ObjectId(id) };
-      const updateDoc = {
-        $set: {
-          paid: true,
-          transactionId: payment.transaction,
-        },
-      };
-      const updateBooking = await bookingCollection.updateOne(
-        filter,
-        updateDoc
-      );
-      const result = await paymentCollection.insertOne(payment);
-      res.send(updateDoc);
-    });
-
-    // parts booking Post
+    // parts booking post
     app.post("/booking", async (req, res) => {
       const parts = req.body;
       const result = await bookingCollection.insertOne(parts);
       res.send(result);
+    });
+
+    // parts booking get
+    app.get("/booking", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const bookings = await bookingCollection.find(query).toArray();
+      return res.send(bookings);
     });
 
     // add review post
@@ -136,66 +90,42 @@ async function run() {
     });
 
     // get users
-    app.get("/user", verifyJWT, async (req, res) => {
+    app.get("/user", async (req, res) => {
       const users = await userCollection.find().toArray();
       res.send(users);
     });
 
-    app.get("/admin/:email", async (req, res) => {
-      const email = req.params.email;
-      const user = await userCollection.findOne({ email: email });
-      const isAdmin = user.role === "admin";
-      res.send({ admin: isAdmin });
-    });
-
     // users admin
-    app.put("/user/admin/:email", verifyJWT, async (req, res) => {
+    app.put("/user/admin/:email", async (req, res) => {
       const email = req.params.email;
-      const requester = req.decoded.email;
-      const requesterAcount = await userCollection.findOne({
-        email: requester,
-      });
-      if (requesterAcount.role === "admin") {
-        const filter = { email: email };
-        const updateDoc = {
-          $set: { role: "admin" },
-        };
-        const result = await userCollection.updateOne(filter, updateDoc);
-        res.send(result);
-      } else {
-        res.status(403).send({ message: "Forbiden Access" });
-      }
+      const filter = { email: email };
+      const updateDoc = {
+        $set: { role: "admin" },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+      return;
     });
 
-    // payment intent
-    app.post("/create-payment-intent", verifyJWT, async (req, res) => {
-      const { price } = req.body;
-      const amount = price * 100;
-      const paymentIntent = await stripe.paymentIntents.create({
-        amount: amount,
-        currency: "usd",
-        payment_method_types: ["card"],
-      });
-      res.send({
-        clientSecret: paymentIntent.client_secret,
-      });
-    });
-
-    // users put
-    app.put("/user/:email", async (req, res) => {
+    //update User
+    app.put("/users/:email", async (req, res) => {
       const email = req.params.email;
       const user = req.body;
       const filter = { email: email };
-      const options = { upsert: true };
+      const option = { upsert: true };
       const updateDoc = {
-        $set: user,
+        $set: { user },
       };
-      const result = await userCollection.updateOne(filter, updateDoc, options);
-      const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN, {
-        expiresIn: "3h",
-      });
+      const result = await userCollection.updateOne(filter, updateDoc, option);
+      res.send(result);
+    });
 
-      res.send({ result, token });
+    // get Update user
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await userCollection.findOne(query);
+      res.send(result);
     });
   } finally {
     //   await client.close()
@@ -211,3 +141,133 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`manufacture app listening on port ${port}`);
 });
+
+// parts gets id
+// app.get("/parts/:id", async (req, res) => {
+//   const id = req.params.id;
+//   const query = { _id: ObjectId(id) };
+//   const purchase = await partsCollection.findOne(query);
+//   res.send(purchase);
+// });
+
+// // parts post
+// app.post("/parts", async (req, res) => {
+//   const parts = req.body;
+//   const result = await partsCollection.insertOne(parts);
+//   res.send(result);
+// });
+
+// // parts booking get
+// app.get("/booking", verifyJWT, async (req, res) => {
+//   const email = req.query.email;
+//   const decodedEmail = req.decoded.email;
+//   if (email === decodedEmail) {
+//     const query = { email: email };
+//     const bookings = await bookingCollection.find(query).toArray();
+//     return res.send(bookings);
+//   } else {
+//     return res.status(403).send({ message: "Forbiden Access" });
+//   }
+// });
+
+// // delete parts
+
+// app.delete("/parts/:id", async (req, res) => {
+//   const id = req.params.id;
+//   const query = { _id: ObjectId(id) };
+//   const result = await partsCollection.deleteOne(query);
+//   res.send(result);
+// });
+
+// app.get("/booking/:id", verifyJWT, async (req, res) => {
+//   const id = req.params.id;
+//   const query = { _id: ObjectId(id) };
+//   const booking = await bookingCollection.findOne(query);
+//   res.send(booking);
+// });
+
+// app.patch("/booking/:id", verifyJWT, async (req, res) => {
+//   const id = req.params.id;
+//   const payment = req.body;
+//   const filter = { _id: ObjectId(id) };
+//   const updateDoc = {
+//     $set: {
+//       paid: true,
+//       transactionId: payment.transaction,
+//     },
+//   };
+//   const updateBooking = await bookingCollection.updateOne(
+//     filter,
+//     updateDoc
+//   );
+//   const result = await paymentCollection.insertOne(payment);
+//   res.send(updateDoc);
+// });
+
+// // parts booking Post
+// app.post("/booking", async (req, res) => {
+//   const parts = req.body;
+//   const result = await bookingCollection.insertOne(parts);
+//   res.send(result);
+// });
+
+// // add review post
+// app.post("/review", async (req, res) => {
+//   const reviews = req.body;
+//   const result = await reviewCollection.insertOne(reviews);
+//   res.send(result);
+// });
+
+// // get review
+// app.get("/review", async (req, res) => {
+//   const query = {};
+//   const cursor = reviewCollection.find(query);
+//   const reviews = await cursor.toArray();
+//   res.send(reviews);
+// });
+
+// // get users
+// app.get("/user", verifyJWT, async (req, res) => {
+//   const users = await userCollection.find().toArray();
+//   res.send(users);
+// });
+
+// app.get("/admin/:email", async (req, res) => {
+//   const email = req.params.email;
+//   const user = await userCollection.findOne({ email: email });
+//   const isAdmin = user.role === "admin";
+//   res.send({ admin: isAdmin });
+// });
+
+// // users admin
+// app.put("/user/admin/:email", verifyJWT, async (req, res) => {
+//   const email = req.params.email;
+//   const requester = req.decoded.email;
+//   const requesterAcount = await userCollection.findOne({
+//     email: requester,
+//   });
+//   if (requesterAcount.role === "admin") {
+//     const filter = { email: email };
+//     const updateDoc = {
+//       $set: { role: "admin" },
+//     };
+//     const result = await userCollection.updateOne(filter, updateDoc);
+//     res.send(result);
+//   } else {
+//     res.status(403).send({ message: "Forbiden Access" });
+//   }
+// });
+
+// // payment intent
+// app.post("/create-payment-intent", verifyJWT, async (req, res) => {
+//   const { price } = req.body;
+//   const amount = price * 100;
+//   const paymentIntent = await stripe.paymentIntents.create({
+//     amount: amount,
+//     currency: "usd",
+//     payment_method_types: ["card"],
+//   });
+//   res.send({
+//     clientSecret: paymentIntent.client_secret,
+//   });
+// });
